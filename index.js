6f9b1cd45a69dab037023423a99fe354417a189d
Gabbie Piraino <pirainogi@gmail.com>
2018-11-13 15:40:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(katzDeliLine, customerName){
   return (`Welcome, ${customerName}. You are number ${katzDeliLine.length} in line.`);
 }
 
-function nowServing(katzDeliLine, customerName){
+function nowServing(katzDeliLine){
   if (katzDeliLine.length === 0){
   return (`There is nobody waiting to be served!`);
   } else {
